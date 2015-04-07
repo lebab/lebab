6825257f80887c18368e21d14baffb877a2d3b71
Mohamad Mohebifar <netfars@gmail.com>
2015-04-07 10:29:45 +0430

    doc: Updated doc block for transformer

--- src/transformer.js
+++ src/transformer.js
@@ -5,10 +5,18 @@ import fs from 'fs';
 export default
 class Transformer {
 
+  /**
+   * @constructor
+   */
   constructor() {
-
+    this.ast = {};
   }
 
+  /**
+   * Prepare the abstract syntax tree for given file
+   *
+   * @param filename
+   */
   readFile(filename) {
     this.ast = astGenerator.readFile(filename, {
       sync: true,
@@ -16,20 +24,46 @@ class Transformer {
     });
   }
 
+  /**
+   * Prepare an abstract syntax tree for given code in string
+   *
+   * @param string
+   */
   read(string) {
     this.ast = astGenerator.read(string);
   }
 
+  /**
+   * Apply a transformation on the AST
+   *
+   * @param transformation
+   */
   applyTransformation(transformation) {
     transformation(this.ast);
   }
 
+  /**
+   * Returns the code string
+   *
+   * @returns {Object}
+   */
   out() {
     return codeGenerator.generate(this.ast);
   }
 
-  writeFile(filename) {
-    fs.writeFileSync(filename, this.out());
+  /**
+   * Writes the code on file
+   *
+   * @param filename
+   */
+  writeFile(filename, callback) {
+    const code = this.out();
+
+    if(typeof callback === 'function') {
+      fs.writeFile(filename, code, callback);
+    } else {
+      fs.writeFileSync(filename, code);
+    }
   }
 
 }
