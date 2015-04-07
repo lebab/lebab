13e602dffb1ce1720e22323e0609c56fa0a581cd
Mohamad Mohebifar <netfars@gmail.com>
2015-04-07 13:09:49 +0430

    test: Add class transformation test

---
+++ test/transformation/classes.js
@@ -0,0 +1,58 @@
+var expect = require('chai').expect;
+var
+  classTransformation = require('./../../lib/transformation/classes'),
+  Transformer = require('./../../lib/transformer'),
+  transformer = new Transformer();
+
+describe('Class transformation', function () {
+
+  it('should not convert functions without prototype assignment to class', function (done) {
+    var script = "function someClass() {\n}";
+
+    transformer.read(script);
+    transformer.applyTransformation(classTransformation);
+
+    expect(transformer.out()).to.equal(script);
+    done();
+  });
+
+  it('should not convert functions with prototype assignment to class', function (done) {
+    var script = "function someClass() {\n}\nsomeClass.prototype.someMethod = function(a, b) {\n}";
+
+    transformer.read(script);
+    transformer.applyTransformation(classTransformation);
+    var result = transformer.out();
+
+    expect(result).to.include('class someClass');
+    expect(result).to.include('someMethod(a, b)');
+    done();
+  });
+
+  it('should apply non-anonymous functions to methods', function (done) {
+    var script = "function someClass() {\n}\nsomeClass.prototype.someMethod = someMethod\nfunction someMethod(a, b) {\n}";
+
+    transformer.read(script);
+    transformer.applyTransformation(classTransformation);
+    var result = transformer.out();
+
+    expect(result).to.include('class someClass');
+    expect(result).to.include('someMethod(a, b)');
+    expect(result).to.include('someMethod.apply(this, arguments)');
+    done();
+  });
+
+  it('should convert Object.defineProperty to setters and getters', function (done) {
+    var script = "function someClass() {\n}\nsomeClass.prototype.someMethod = function (a, b) {\n}\nObject.defineProperty(someClass.prototype, 'someAccessor', {\nget: function () {\nreturn this._some;\n},\nset: function (value) {\nthis._some = value;\n}\n});";
+
+    transformer.read(script);
+    transformer.applyTransformation(classTransformation);
+    var result = transformer.out();
+
+    expect(result).to.include('class someClass');
+    expect(result).to.include('someMethod(a, b)');
+    expect(result).to.include('get someAccessor()');
+    expect(result).to.include('set someAccessor(value)');
+    done();
+  });
+
+});
