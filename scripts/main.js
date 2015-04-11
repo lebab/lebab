7c3e13efffae00ca811689e553e3f9ca6147b94a
mohamad <netfars@gmail.com>
2015-04-11 23:46:00 +0430

    Change the design

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,15 @@
-// var editor = ace.edit("editor");
-// editor.setTheme("ace/theme/github");
-// editor.getSession().setMode("ace/mode/javascript");
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/github");
+editor.getSession().setMode("ace/mode/javascript");
 
-var result = document.getElementById('result'),
-    source = document.getElementById('source');
-var transformer = new Transformer();
+var resultElement = document.getElementById('result');
+var transformer = new Transform();
 
 function transpile() {
-  transformer.read(source.textContent);
+  transformer.read(editor.getSession().getValue());
   transformer.applyTransformations();
-  result.innerHTML = transformer.out();
-  // hljs.highlightBlock(result);
+  resultElement.innerHTML = transformer.out();
+  hljs.highlightBlock(resultElement);
 }
-
-var timeout = false;
-source.addEventListener('keyup', function(e) {
-  if (timeout) clearTimeout(timeout);
-  timeout = setTimeout(function() {
-    transpile();
-    timeout = false;
-  }, 500);
-});
 
 transpile();
