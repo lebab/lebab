3d272c3e36826d1fc9534582395c3e6a33881eb5
mohamad <netfars@gmail.com>
2015-04-12 00:30:10 +0430

    Fix try-live

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ editor.setTheme("ace/theme/github");
 editor.getSession().setMode("ace/mode/javascript");
 
 var resultElement = document.getElementById('result');
-var transformer = new Transform();
+var transformer = new Transformer();
 
 function transpile() {
   transformer.read(editor.getSession().getValue());
@@ -11,5 +11,18 @@ function transpile() {
   resultElement.innerHTML = transformer.out();
   hljs.highlightBlock(resultElement);
 }
+
+var timeout = false;
+
+editor.on('input', function () {
+  if (timeout) {
+    clearTimeout(timeout);
+  }
+
+  timeout = setTimeout(function () {
+    transpile();
+    timeout = false;
+  }, 500);
+});
 
 transpile();
