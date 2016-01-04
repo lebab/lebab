5f67d421c55119b3dc9c22f7bea70114b465b335
Rene Saarsoo <nene@triin.net>
2016-01-04 22:09:27 +0200

    Add --transformers command line option.
    Allows specifying a list of transforms like so:
    
        lebab --transformers let,arrowFunction
    
    which results in lebab only performing let and arrowFunction
    transforms, disabling all other transformiers.
    
    When unknown transformer name specified, prints out a warning message.

--- bin/file.js
+++ bin/file.js
@@ -1,12 +1,34 @@
 var Transformer = require('./../lib/transformer');
+var _ = require('lodash');
 
 module.exports = function (program, file) {
+  // Enable all transformers by default
   var options = {
-    transformers: {}
+    transformers: {
+      classes: true,
+      stringTemplates: true,
+      arrowFunctions: true,
+      let: true,
+      defaultArguments: true,
+      objectMethods: true
+    }
   };
 
+  // When --no-classes used, disable classes transformer
   if(! program.classes) {
     options.transformers.classes = false;
+  }
+
+  // When --transformers used turn off everything besides the specified tranformers
+  if (program.transformers) {
+    options.transformers = _.mapValues(options.transformers, _.constant(false));
+
+    program.transformers.forEach(function (name) {
+      if (!options.transformers.hasOwnProperty(name)) {
+        console.error("Unknown transformer '" + name + "'.");
+      }
+      options.transformers[name] = true;
+    });
   }
 
   var transformer = new Transformer(options);
