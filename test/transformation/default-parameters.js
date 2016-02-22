319faca16e510229aed6007ad919f2bc3a0ef510
Rene Saarsoo <nene@triin.net>
2016-02-22 20:30:41 +0200

    Properly indent code in default-params tests

--- test/transformation/default-parameters.js
+++ test/transformation/default-parameters.js
@@ -9,26 +9,44 @@ function test(script) {
 describe('Default parameters', function () {
 
   it('should work for simple, single argument function declarations', function () {
-    var script = 'function x(a) { a = a || 2 }';
-
-    expect(test(script)).to.equal('function x(a=2) {}');
+    expect(test(
+      'function x(a) {\n' +
+      '  a = a || 2;\n' +
+      '}'
+    )).to.equal(
+      'function x(a=2) {}'
+    );
   });
 
   it('should work for different types of arguments', function () {
-    var script = 'function x(a, b, c) { a = a || \'salam\'; b = b || {}; c = c || [] }';
-
-    expect(test(script)).to.equal('function x(a=\'salam\', b={}, c=[]) {}');
+    expect(test(
+      'function x(a, b, c) {\n' +
+      '  a = a || "salam";\n' +
+      '  b = b || {};\n' +
+      '  c = c || [];\n'+
+      '}'
+    )).to.equal(
+      'function x(a="salam", b={}, c=[]) {}'
+    );
   });
 
   it('should work for multiple functions', function () {
-    var script = 'function x(a) { a = a || 2 }\nfunction y(a) { a = a || 3 }';
-
-    expect(test(script)).to.equal('function x(a=2) {}\nfunction y(a=3) {}');
+    expect(test(
+      'function x(a) { a = a || 2; }\n' +
+      'function y(a) { a = a || 3; }'
+    )).to.equal(
+      'function x(a=2) {}\n' +
+      'function y(a=3) {}'
+    );
   });
 
   it('should work for function expressions', function () {
-    var script = 'b(function(a) { a = a || 2 });';
-
-    expect(test(script)).to.equal('b(function(a=2) {});');
+    expect(test(
+      '(function(a) {\n' +
+      '  a = a || 2;\n' +
+      '})();'
+    )).to.equal(
+      '(function(a=2) {})();'
+    );
   });
 });
