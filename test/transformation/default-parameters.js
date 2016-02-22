c5c932fd516ad69b2e8a338097fb244cb5f523e6
Rene Saarsoo <nene@triin.net>
2016-02-22 20:44:46 +0200

    Additional tests for default parameters
    - Extra tests for corner cases.
    - Disabled tests for known bugs.

--- test/transformation/default-parameters.js
+++ test/transformation/default-parameters.js
@@ -6,9 +6,13 @@ function test(script) {
   return transformer.run(script);
 }
 
+function expectNoChange(script) {
+  expect(test(script)).to.equal(script);
+}
+
 describe('Default parameters', function () {
 
-  it('should work for simple, single argument function declarations', function () {
+  it('should work for simple, single parameter function declarations', function () {
     expect(test(
       'function x(a) {\n' +
       '  a = a || 2;\n' +
@@ -18,7 +22,7 @@ describe('Default parameters', function () {
     );
   });
 
-  it('should work for different types of arguments', function () {
+  it('should work for different types of parameters', function () {
     expect(test(
       'function x(a, b, c) {\n' +
       '  a = a || "salam";\n' +
@@ -27,6 +31,16 @@ describe('Default parameters', function () {
       '}'
     )).to.equal(
       'function x(a="salam", b={}, c=[]) {}'
+    );
+  });
+
+  it('should work when only some parameters have defaults', function () {
+    expect(test(
+      'function x(a, b, c) {\n' +
+      '  b = b || 3;\n' +
+      '}'
+    )).to.equal(
+      'function x(a, b=3, c) {}'
     );
   });
 
@@ -40,13 +54,81 @@ describe('Default parameters', function () {
     );
   });
 
-  it('should work for function expressions', function () {
+  xit('should work for nested functions with similar parameter names', function () {
     expect(test(
-      '(function(a) {\n' +
+      'function x(a) {\n' +
+      '  function y(a) {\n' +
+      '    a = a || 3;\n' +
+      '  }\n' +
       '  a = a || 2;\n' +
-      '})();'
+      '}'
     )).to.equal(
-      '(function(a=2) {})();'
+      'function x(a=2) {\n' +
+      '  function y(a=3) {}\n' +
+      '}'
     );
   });
+
+  xit('should ignore parameters modified before assigning default', function () {
+    expectNoChange(
+      'function x(a) {\n' +
+      '  a = foo();\n' +
+      '  a = a || 2;\n' +
+      '}'
+    );
+  });
+
+  xit('should ignore parameters when any other code executed before assigning default', function () {
+    expectNoChange(
+      'function x(a) {\n' +
+      '  foo();\n' +
+      '  a = a || 2;\n' +
+      '  function foo() {\n' +
+      '    a++;\n' +
+      '  }\n' +
+      '}'
+    );
+  });
+
+  it('should work for function expressions', function () {
+    expect(test(
+      'foo(function(a) {\n' +
+      '  a = a || 2;\n' +
+      '});'
+    )).to.equal(
+      'foo(function(a=2) {});'
+    );
+  });
+
+  xit('should work for arrow function expressions', function () {
+    expect(test(
+      'foo((a) => {\n' +
+      '  a = a || 2;\n' +
+      '});'
+    )).to.equal(
+      'foo((a=2) => {});'
+    );
+  });
+
+  xit('should preserve existing default parameters', function () {
+    expectNoChange(
+      'function x(a="salam", b={}, c=[]) {}'
+    );
+  });
+
+  xit('should not override existing default parameters', function () {
+    expectNoChange(
+      'function x(a=25) {\n' +
+      '  a = a || 2;\n' +
+      '}'
+    );
+  });
+
+  // Regression test for issue #89
+  xit('should preserve long parameter lists as is when there are no defaults', function () {
+    expectNoChange(
+      'function foo(variable1, variable2, variable3, variable4, variable5, variable6, variable7, variable8) {}'
+    );
+  });
+
 });
