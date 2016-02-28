e6a0648f90614f3e490f2f84719b017a26bd1e24
Rene Saarsoo <nene@triin.net>
2016-02-28 13:46:48 +0200

    Extend matchesAst() to support extracting fields
    Instead of returning true when match found, the matcher created by
    matchesAst() will return an object with extracted fields - these
    are marked inside the object pattern using extract() utility.
    
    This allows us to combine the matching and extracting steps into
    one simpler expression, removing the need to write bunch of code
    to extract values that interest us.

--- src/utils/matches-ast.js
+++ src/utils/matches-ast.js
@@ -3,19 +3,68 @@ import _ from 'lodash';
 /**
  * Creates a function that matches AST against the given pattern,
  *
- * Just like _.matches(), but with the addition that a Function
+ * Similar to LoDash.matches(), but with the addition that a Function
  * can be provided to assert various conditions e.g. checking
  * that an array must be of a specific length.
  *
+ * Additionally the utility extract() can be used to give names to
+ * the parts of AST - these are then returned as a map of key-value
+ * pairs.
+ *
  * @param  {Object}  pattern
- * @return {Function}
+ * @return {Function} Function that returns an object with
+ * extracted fields or false when no match found.
  */
-export default function(pattern) {
+export function matchesAst(pattern) {
   return (ast) => {
-    return _.isMatchWith(ast, pattern, (value, matcher) => {
+    const extractedFields = {};
+
+    const matches = _.isMatchWith(ast, pattern, (value, matcher) => {
       if (typeof matcher === 'function') {
-        return matcher(value);
+        const result = matcher(value);
+        if (typeof result === 'object') {
+          Object.assign(extractedFields, result);
+        }
+        return result;
       }
     });
+
+    if (matches) {
+      return extractedFields;
+    }
+    else {
+      return false;
+    }
   };
 }
+
+/**
+ * Utility for extracting values during matching with matchesAst()
+ *
+ * @param {String} fieldName The name to give for the value
+ * @param {Function|Object} matcher Optional matching function or pattern for matchesAst()
+ * @return {Function}
+ */
+export function extract(fieldName, matcher) {
+  return (ast) => {
+    let extractedFields = {[fieldName]: ast};
+
+    if (typeof matcher === 'object') {
+      matcher = matchesAst(matcher);
+    }
+
+    if (typeof matcher === 'function') {
+      let result = matcher(ast);
+      if (typeof result === 'object') {
+        return Object.assign(extractedFields, result);
+      }
+      if (!result) {
+        return false;
+      }
+    }
+
+    return extractedFields;
+  };
+}
+
+export default matchesAst;
