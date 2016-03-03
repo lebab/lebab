c477e8eabbf0ce4bba13f7302f15e0dc33293610
Rene Saarsoo <nene@triin.net>
2016-03-03 19:50:34 +0200

    Organize commonjs tests to default/named sections

--- test/transform/commonjs/export-commonjs.js
+++ test/transform/commonjs/export-commonjs.js
@@ -12,36 +12,38 @@ function expectNoChange(script) {
 
 describe('Export CommonJS', () => {
 
-  it('should convert module.exports assignment to default export', () => {
-    expect(test('module.exports = 123;')).to.equal('export default 123;');
-    expect(test('module.exports = function() {};')).to.equal('export default function() {};');
-    expect(test('module.exports = x => x;')).to.equal('export default x => x;');
-    expect(test('module.exports = class {};')).to.equal('export default class {};');
-  });
+  describe('default export', () => {
+    it('should convert module.exports assignment to default export', () => {
+      expect(test('module.exports = 123;')).to.equal('export default 123;');
+      expect(test('module.exports = function() {};')).to.equal('export default function() {};');
+      expect(test('module.exports = x => x;')).to.equal('export default x => x;');
+      expect(test('module.exports = class {};')).to.equal('export default class {};');
+    });
 
-  it('should not convert assignment to exports', () => {
-    expectNoChange('exports = function() {};');
-  });
+    it('should not convert assignment to exports', () => {
+      expectNoChange('exports = function() {};');
+    });
 
-  it('should not convert weird assignment to module.exports', () => {
-    expectNoChange('module.exports += function() {};');
-    expectNoChange('module.exports -= function() {};');
-    expectNoChange('module.exports *= function() {};');
-    expectNoChange('module.exports /= function() {};');
-  });
+    it('should not convert weird assignment to module.exports', () => {
+      expectNoChange('module.exports += function() {};');
+      expectNoChange('module.exports -= function() {};');
+      expectNoChange('module.exports *= function() {};');
+      expectNoChange('module.exports /= function() {};');
+    });
 
-  // A pretty weird thing to do...
-  // shouldn't bother supporting it.
-  it('should not convert assignment to module["exports"]', () => {
-    expectNoChange('module["exports"] = function() {};');
-  });
+    // A pretty weird thing to do...
+    // shouldn't bother supporting it.
+    it('should not convert assignment to module["exports"]', () => {
+      expectNoChange('module["exports"] = function() {};');
+    });
 
-  it('should ignore module.exports inside statements', () => {
-    expectNoChange(
-      'if (true) {\n' +
-      '  module.exports = function() {};\n' +
-      '}'
-    );
+    it('should ignore module.exports inside statements', () => {
+      expectNoChange(
+        'if (true) {\n' +
+        '  module.exports = function() {};\n' +
+        '}'
+      );
+    });
   });
 
 });
--- test/transform/commonjs/import-commonjs.js
+++ test/transform/commonjs/import-commonjs.js
@@ -12,90 +12,94 @@ function expectNoChange(script) {
 
 describe('Import CommonJS', () => {
 
-  it('should convert basic var/let/const with require()', () => {
-    expect(test('var   foo = require("foo");')).to.equal('import foo from "foo";');
-    expect(test('const foo = require("foo");')).to.equal('import foo from "foo";');
-    expect(test('let   foo = require("foo");')).to.equal('import foo from "foo";');
+  describe('default import', () => {
+    it('should convert basic var/let/const with require()', () => {
+      expect(test('var   foo = require("foo");')).to.equal('import foo from "foo";');
+      expect(test('const foo = require("foo");')).to.equal('import foo from "foo";');
+      expect(test('let   foo = require("foo");')).to.equal('import foo from "foo";');
+    });
+
+    it('should do nothing with var that contains no require()', () => {
+      expectNoChange('var foo = "bar";');
+      expectNoChange('var foo;');
+    });
+
+    it('should do nothing with require() that does not have a single string argument', () => {
+      expectNoChange('var foo = require();');
+      expectNoChange('var foo = require("foo", {});');
+      expectNoChange('var foo = require(bar);');
+      expectNoChange('var foo = require(123);');
+    });
+
+    it('should convert var with multiple require() calls', () => {
+      expect(test(
+        'var foo = require("foo"), bar = require("bar");'
+      )).to.equal(
+        'import foo from "foo";\n' +
+        'import bar from "bar";'
+      );
+    });
+
+    it('should convert var/let/const with intermixed require() calls and normal initializations', () => {
+      expect(test(
+        'var foo = require("foo"), bar = 15;'
+      )).to.equal(
+        'import foo from "foo";\n' +
+        'var bar = 15;'
+      );
+
+      expect(test(
+        'let abc, foo = require("foo")'
+      )).to.equal(
+        'let abc;\n' +
+        'import foo from "foo";'
+      );
+
+      expect(test(
+        'const greeting = "hello", foo = require("foo");'
+      )).to.equal(
+        'const greeting = "hello";\n' +
+        'import foo from "foo";'
+      );
+    });
+
+    // It would be nice to preserve the combined declarations,
+    // but this kind of intermixed vars should really be a rare edge case.
+    it('does not need to preserve combined variable declarations', () => {
+      expect(test(
+        'var foo = require("foo"), bar = 1, baz = 2;'
+      )).to.equal(
+        'import foo from "foo";\n' +
+        'var bar = 1;\n' +
+        'var baz = 2;'
+      );
+    });
+
+    it('should ignore require calls inside statements', () => {
+      expectNoChange(
+        'if (true) {\n' +
+        '  var foo = require("foo");\n' +
+        '}'
+      );
+    });
   });
 
-  it('should do nothing with var that contains no require()', () => {
-    expectNoChange('var foo = "bar";');
-    expectNoChange('var foo;');
-  });
+  describe('named import', () => {
+    it('should convert foo = require().foo to named import', () => {
+      expect(test(
+        'var foo = require("foolib").foo;'
+      )).to.equal(
+        'import {foo} from "foolib";'
+      );
+    });
 
-  it('should do nothing with require() that does not have a single string argument', () => {
-    expectNoChange('var foo = require();');
-    expectNoChange('var foo = require("foo", {});');
-    expectNoChange('var foo = require(bar);');
-    expectNoChange('var foo = require(123);');
-  });
-
-  it('should convert var with multiple require() calls', () => {
-    expect(test(
-      'var foo = require("foo"), bar = require("bar");'
-    )).to.equal(
-      'import foo from "foo";\n' +
-      'import bar from "bar";'
-    );
-  });
-
-  it('should convert var/let/const with intermixed require() calls and normal initializations', () => {
-    expect(test(
-      'var foo = require("foo"), bar = 15;'
-    )).to.equal(
-      'import foo from "foo";\n' +
-      'var bar = 15;'
-    );
-
-    expect(test(
-      'let abc, foo = require("foo")'
-    )).to.equal(
-      'let abc;\n' +
-      'import foo from "foo";'
-    );
-
-    expect(test(
-      'const greeting = "hello", foo = require("foo");'
-    )).to.equal(
-      'const greeting = "hello";\n' +
-      'import foo from "foo";'
-    );
-  });
-
-  // It would be nice to preserve the combined declarations,
-  // but this kind of intermixed vars should really be a rare edge case.
-  it('does not need to preserve combined variable declarations', () => {
-    expect(test(
-      'var foo = require("foo"), bar = 1, baz = 2;'
-    )).to.equal(
-      'import foo from "foo";\n' +
-      'var bar = 1;\n' +
-      'var baz = 2;'
-    );
-  });
-
-  it('should ignore require calls inside statements', () => {
-    expectNoChange(
-      'if (true) {\n' +
-      '  var foo = require("foo");\n' +
-      '}'
-    );
-  });
-
-  it('should convert foo = require().foo to named import', () => {
-    expect(test(
-      'var foo = require("foolib").foo;'
-    )).to.equal(
-      'import {foo} from "foolib";'
-    );
-  });
-
-  it('should convert bar = require().foo to aliased named import', () => {
-    expect(test(
-      'var bar = require("foolib").foo;'
-    )).to.equal(
-      'import {foo as bar} from "foolib";'
-    );
+    it('should convert bar = require().foo to aliased named import', () => {
+      expect(test(
+        'var bar = require("foolib").foo;'
+      )).to.equal(
+        'import {foo as bar} from "foolib";'
+      );
+    });
   });
 
   // Not yet supported things...
