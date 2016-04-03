c29cb7a96bf7fb64922ac239a8b175d346e415d5
Rene Saarsoo <nene@triin.net>
2016-04-03 12:14:59 +0300

    Move splitting logic out of TemplateLiteral
    Keep the syntax class simple, move the quases/expressions splitting
    logic into the main template transform file.

--- src/syntax/template-literal.js
+++ src/syntax/template-literal.js
@@ -1,6 +1,4 @@
 import BaseSyntax from './base';
-import TemplateElement from './template-element';
-import isString from './../utils/is-string';
 
 /**
  * The class to define the TemplateLiteral syntax
@@ -9,57 +7,13 @@ export default
 class TemplateLiteral extends BaseSyntax {
   /**
    * Create a template literal
-   * @param {Object[]} parts
+   * @param {Object[]} quasis String parts
+   * @param {Object[]} expressions Expressions between string parts
    */
-  constructor(parts) {
+  constructor({quasis, expressions}) {
     super('TemplateLiteral');
 
-    this.expressions = [];
-    this.quasis = [];
-
-    this.createFromArray(parts);
-  }
-
-  createFromArray(parts) {
-    for (let i = 0; i < parts.length; i++) {
-      const curr = parts[i];
-
-      if (isString(curr)) {
-        let currVal = curr.value;
-        let currRaw = this.escapeForTemplate(curr.raw);
-
-        while (isString(parts[i + 1] || {})) {
-          i++;
-          currVal += parts[i].value;
-          currRaw += this.escapeForTemplate(parts[i].raw);
-        }
-
-        this.quasis.push(new TemplateElement({
-          raw: currRaw,
-          cooked: currVal
-        }));
-      }
-      else {
-        if (i === 0) {
-          this.quasis.push(new TemplateElement({}));
-        }
-
-        if (!isString(parts[i + 1] || {})) {
-          this.quasis.push(new TemplateElement({
-            tail: typeof parts[i + 1] === 'undefined'
-          }));
-        }
-
-        this.expressions.push(curr);
-      }
-    }
-  }
-
-  // Strip surrounding quotes, escape backticks and unescape escaped quotes
-  escapeForTemplate(raw) {
-    return raw
-      .replace(/^['"]|['"]$/g, '')
-      .replace(/`/g, '\\`')
-      .replace(/\\(['"])/g, '$1');
+    this.quasis = quasis;
+    this.expressions = expressions;
   }
 }
--- src/transform/template.js
+++ src/transform/template.js
@@ -1,5 +1,6 @@
 import estraverse from 'estraverse';
 import TemplateLiteral from './../syntax/template-literal';
+import TemplateElement from './../syntax/template-element';
 import isString from './../utils/is-string';
 import _ from 'lodash';
 
@@ -11,7 +12,7 @@ export default function(ast) {
 
         if (operands.some(isString)) {
           this.skip();
-          return new TemplateLiteral(operands);
+          return new TemplateLiteral(splitQuasisAndExpressions(operands));
         }
       }
     }
@@ -32,4 +33,52 @@ function detectOperands(node) {
 
 function isPlusExpression(node) {
   return node.type === 'BinaryExpression' && node.operator === '+';
+}
+
+function splitQuasisAndExpressions(operands) {
+  const quasis = [];
+  const expressions = [];
+
+  for (let i = 0; i < operands.length; i++) {
+    const curr = operands[i];
+
+    if (isString(curr)) {
+      let currVal = curr.value;
+      let currRaw = escapeForTemplate(curr.raw);
+
+      while (isString(operands[i + 1] || {})) {
+        i++;
+        currVal += operands[i].value;
+        currRaw += escapeForTemplate(operands[i].raw);
+      }
+
+      quasis.push(new TemplateElement({
+        raw: currRaw,
+        cooked: currVal
+      }));
+    }
+    else {
+      if (i === 0) {
+        quasis.push(new TemplateElement({}));
+      }
+
+      if (!isString(operands[i + 1] || {})) {
+        quasis.push(new TemplateElement({
+          tail: typeof operands[i + 1] === 'undefined'
+        }));
+      }
+
+      expressions.push(curr);
+    }
+  }
+
+  return {quasis, expressions};
+}
+
+// Strip surrounding quotes, escape backticks and unescape escaped quotes
+function escapeForTemplate(raw) {
+  return raw
+    .replace(/^['"]|['"]$/g, '')
+    .replace(/`/g, '\\`')
+    .replace(/\\(['"])/g, '$1');
 }
