a3f720a8396f08043e28804242fe5e6523a189cf
Rene Saarsoo <nene@triin.net>
2016-06-03 11:15:15 +0300

    Add failing test for JSX support

---
+++ test/transform/jsx.js
@@ -0,0 +1,26 @@
+import {expect} from 'chai';
+import Transformer from './../../lib/transformer';
+const transformer = new Transformer({
+  'class': true,
+  'template': true,
+  'arrow': true,
+  'let': true,
+  'default-param': true,
+  'arg-spread': true,
+  'obj-method': true,
+  'obj-shorthand': true,
+  'no-strict': true,
+  'commonjs': true,
+});
+
+function expectNoChange(script) {
+  expect(transformer.run(script)).to.equal(script);
+}
+
+describe('JSX support', () => {
+  it('ignores JSX syntax', () => {
+    expectNoChange(
+      'var foo = ( <div/> );'
+    );
+  });
+});
