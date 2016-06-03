1455996f88eccfa5872cc279da83667766667b12
Rene Saarsoo <nene@triin.net>
2016-06-03 13:47:28 +0300

    JSX support for ESTraverse
    Extend ESTraverse to support JSX AST node types.
    Added tests for the full range of JSX syntax.
    
    With this, Lebab should now work fine with code containing JSX.
    
    Hurray!

--- src/traverser.js
+++ src/traverser.js
@@ -1,5 +1,22 @@
 import estraverse from 'estraverse';
 
+// JSX AST types, as documented in:
+// https://github.com/facebook/jsx/blob/master/AST.md
+const jsxExtensions = {
+  keys: {
+    JSXIdentifier: [],
+    JSXMemberExpression: ['object', 'property'],
+    JSXNamespacedName: ['namespace', 'name'],
+    JSXEmptyExpression: [],
+    JSXExpressionContainer: ['expression'],
+    JSXOpeningElement: ['name', 'attributes'],
+    JSXClosingElement: ['name'],
+    JSXAttribute: ['name', 'value'],
+    JSXSpreadAttribute: ['argument'],
+    JSXElement: ['openingElement', 'closingElement', 'children'],
+  }
+};
+
 /**
  * Proxy for ESTraverse.
  * Providing a single place to easily extend its functionality.
@@ -8,10 +25,10 @@ import estraverse from 'estraverse';
  */
 export default {
   traverse(tree, cfg) {
-    return estraverse.traverse(tree, cfg);
+    return estraverse.traverse(tree, Object.assign(cfg, jsxExtensions));
   },
   replace(tree, cfg) {
-    return estraverse.replace(tree, cfg);
+    return estraverse.replace(tree, Object.assign(cfg, jsxExtensions));
   },
   VisitorOption: estraverse.VisitorOption,
 };
--- test/transform/jsx.js
+++ test/transform/jsx.js
@@ -13,14 +13,78 @@ const transformer = new Transformer({
   'commonjs': true,
 });
 
-function expectNoChange(script) {
-  expect(transformer.run(script)).to.equal(script);
+function test(script) {
+  return transformer.run(script);
 }
 
 describe('JSX support', () => {
-  it.only('ignores JSX syntax', () => {
-    expectNoChange(
-      'var foo = ( <div/> );'
+  it('should support self-closing element', () => {
+    expect(test(
+      'var foo = <div/>;'
+    )).to.equal(
+      'const foo = <div/>;'
+    );
+  });
+
+  it('should support attributes', () => {
+    expect(test(
+      'var foo = <div foo="hello" bar={2}/>;'
+    )).to.equal(
+      'const foo = <div foo="hello" bar={2}/>;'
+    );
+  });
+
+  it('should support spread attributes', () => {
+    expect(test(
+      'var foo = <div {...attrs}/>;'
+    )).to.equal(
+      'const foo = <div {...attrs}/>;'
+    );
+  });
+
+  it('should support nested elements', () => {
+    expect(test(
+      'var foo = <div>\n' +
+      '    <Foo/>\n' +
+      '    <Bar/>\n' +
+      '</div>;'
+    )).to.equal(
+      'const foo = <div>\n' +
+      '    <Foo/>\n' +
+      '    <Bar/>\n' +
+      '</div>;'
+    );
+  });
+
+  it('should support member-expressions as element name', () => {
+    expect(test(
+      'var foo = <Foo.bar/>;'
+    )).to.equal(
+      'const foo = <Foo.bar/>;'
+    );
+  });
+
+  it('should support XML namespaces', () => {
+    expect(test(
+      'var foo = <xml:foo/>;'
+    )).to.equal(
+      'const foo = <xml:foo/>;'
+    );
+  });
+
+  it('should support content', () => {
+    expect(test(
+      'var foo = <div>Hello {a + b}</div>;'
+    )).to.equal(
+      'const foo = <div>Hello {a + b}</div>;'
+    );
+  });
+
+  it('should support empty content expressions', () => {
+    expect(test(
+      'var foo = <div> {/* some comments */} </div>;'
+    )).to.equal(
+      'const foo = <div> {/* some comments */} </div>;'
     );
   });
 });
