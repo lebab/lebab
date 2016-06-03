f23b836a7de5951627f09ff168525a45eaf9669b
Rene Saarsoo <nene@triin.net>
2016-06-03 11:10:24 +0300

    Switch to Espree parser with JSX parsing enabled
    Created separate parser module that exposes Esprima-like parse()
    method, inside which it configures the parser.

---
+++ src/parser.js
@@ -0,0 +1,10 @@
+import espree from 'espree';
+
+/**
+ * An Esprima-compatible parser with JSX parsing enabled.
+ */
+export default {
+  parse(js, opts) {
+    return espree.parse(js, Object.assign({ecmaFeatures: {jsx: true}}, opts));
+  }
+};
