ea26157fecdbf3579bd443cb0504d8c6e451f2c2
Rene Saarsoo <nene@triin.net>
2016-06-19 17:40:48 +0300

    Add test helpers for warnings

--- test/createTestHelpers.js
+++ test/createTestHelpers.js
@@ -4,29 +4,41 @@ import Transformer from './../lib/transformer';
 /**
  * Generates functions that are used in all transform-tests.
  * @param  {Object} transformsCfg Config for Transformer class
- * @return {Object} functions expect(), test() and expectNoChange()
+ * @return {Object} functions expectTransform() and expectNoChange()
  */
 export default function(transformsCfg) {
   const transformer = new Transformer(transformsCfg);
 
-  return {
-    // Generic transformation asserter, to be called like:
-    //
-    //   expectTransform("code").toReturn("transformed code");
-    //
-    expectTransform(script) {
-      const code = transformer.run(script).code;
+  // Generic transformation asserter, to be called like:
+  //
+  //   expectTransform("code")
+  //     .toReturn("transformed code");
+  //     .withWarnings(["My warning"]);
+  //
+  function expectTransform(script) {
+    const {code, warnings} = transformer.run(script);
 
-      return {
-        toReturn(expectedValue) {
-          expect(code).to.equal(expectedValue);
-        }
-      };
-    },
+    return {
+      toReturn(expectedValue) {
+        expect(code).to.equal(expectedValue);
+        return this;
+      },
+      withWarnings(expectedWarnings) {
+        expect(warnings).to.deep.equal(expectedWarnings);
+        return this;
+      }
+    };
+  }
 
-    // Asserts that transfroming the string has no effect
-    expectNoChange(script) {
-      expect(transformer.run(script).code).to.equal(script);
-    }
-  };
+  // Asserts that transforming the string has no effect,
+  // and also allows to check for warnings like so:
+  //
+  //   expectNoChange("code")
+  //     .withWarnings(["My warning"]);
+  //
+  function expectNoChange(script) {
+    return expectTransform(script).toReturn(script);
+  }
+
+  return {expectTransform, expectNoChange};
 }
