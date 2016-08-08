d51ee22d05bbc2a12104ad5119e88a9eee00ecce
Christopher Jacobs <apexearth@gmail.com>
2016-08-08 22:04:36 +0000

    Add test should not convert constructor to super() when this is not used. Tweak other test descriptions.

--- test/transform/class-inheritance.js
+++ test/transform/class-inheritance.js
@@ -4,7 +4,7 @@ const {expectTransform, expectNoChange} = createTestHelpers(['class']);
 export default function inheritanceTests() {
   describe('Inheritance', () => {
     describe('node.js require(\'util\').inherits', () => {
-      it('should determine a function is a class when util.inherits is used', () => {
+      it('determines a function is a class when require(\'util\').inherits is used', () => {
         expectTransform(
           'var util2 = require(\'util\');\n' +
           'function MyClass() {\n' +
@@ -14,6 +14,9 @@ export default function inheritanceTests() {
           'var util2 = require(\'util\');\n' +
           'class MyClass extends OtherClass {}'
         );
+      });
+
+      it('tracks assignment of require(\'util\').inherits', () => {
         expectTransform(
           'var inherits2 = require(\'util\').inherits;\n' +
           'function MyClass() {\n' +
@@ -25,7 +28,7 @@ export default function inheritanceTests() {
         );
       });
 
-      it('should preserve util.inherits inheritance when the inherited class is a member expression', () => {
+      it('preserves inheritance when the inherited class is a member expression', () => {
         expectTransform(
           'var util = require(\'util\');\n' +
           'function MyClass() {\n' +
@@ -70,7 +73,7 @@ export default function inheritanceTests() {
         );
       });
 
-      it('should convert constructor calls to super()', () => {
+      it('should convert constructor .call(this, args...) to super()', () => {
         expectTransform(
           'function MyClass(name) {\n' +
           '  OtherClass.call(this, name);\n' +
@@ -82,6 +85,25 @@ export default function inheritanceTests() {
           'class MyClass extends OtherClass {\n' +
           '  constructor(name) {\n' +
           '    super(name);\n' +
+          '    this.name = name;\n' +
+          '  }\n' +
+          '}'
+        );
+      });
+
+      it('should not convert constructor .call(args...) to super() ', () => {
+        // If we call the parent constructor with no `this` then we will not convert to a super call.
+        expectTransform(
+          'function MyClass(name) {\n' +
+          '  OtherClass.call(null, name);\n' +
+          '  this.name = name;\n' +
+          '}\n' +
+          'MyClass.prototype = new OtherClass();\n' +
+          'MyClass.prototype.constructor = MyClass;'
+        ).toReturn(
+          'class MyClass extends OtherClass {\n' +
+          '  constructor(name) {\n' +
+          '    OtherClass.call(null, name);\n' +
           '    this.name = name;\n' +
           '  }\n' +
           '}'
