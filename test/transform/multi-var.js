958b1a0cfab96a0a1a3a83b7f00ae9512e6f9c06
e-cloud <saintscott119@gmail.com>
2016-08-11 09:47:36 +0800

    test: improve to cover more cases for `multi-var` transform

--- test/transform/multi-var.js
+++ test/transform/multi-var.js
@@ -1,7 +1,45 @@
 import createTestHelpers from '../createTestHelpers';
-const {expectTransform} = createTestHelpers(['split-declaration']);
+const {expectTransform} = createTestHelpers(['multi-var']);
 
-describe('Split Declaration', () => {
+describe('Multi-var', () => {
+  describe('with only one variable per declaration', () => {
+    it('should not change anything(one declaration)', () => {
+      expectTransform(
+        'var x;'
+      ).toReturn(
+        'var x;'
+      );
+    });
+
+    it('should not change anything(multi declarations on one line)', () => {
+      expectTransform(
+        'var x;let y;'
+      ).toReturn(
+        'var x;let y;'
+      );
+
+      expectTransform(
+        'var x;let y;const a=100'
+      ).toReturn(
+        'var x;let y;const a=100'
+      );
+    });
+
+    it('should not change anything(multi declarations on multi lines)', () => {
+      expectTransform(
+        'var x;\nlet y;'
+      ).toReturn(
+        'var x;\nlet y;'
+      );
+
+      expectTransform(
+        'var x;\nlet y;\nconst a=100'
+      ).toReturn(
+        'var x;\nlet y;\nconst a=100'
+      );
+    });
+  });
+
   describe('with only uninitialized variables', () => {
     it('should split into separate declarations', () => {
       expectTransform(
@@ -28,6 +66,23 @@ describe('Split Declaration', () => {
         'var x,y=100;let a,b=123;const c=12,d=234'
       ).toReturn(
         'var x;\nvar y=100;\nlet a;\nlet b=123;\nconst c=12;\nconst d=234;'
+      );
+    });
+  });
+
+  describe('with multi lines of declarations', () => {
+    it('should split into separate declarations', () => {
+      expectTransform(
+        'var x,y=100;\n' +
+        'let a,b=123;\n' +
+        'const c=12,d=234'
+      ).toReturn(
+        'var x;\n' +
+        'var y=100;\n' +
+        'let a;\n' +
+        'let b=123;\n' +
+        'const c=12;\n' +
+        'const d=234;'
       );
     });
   });
