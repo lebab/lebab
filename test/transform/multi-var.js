ca6af7872f6517f39753ba680f30dce3a2a3c98e
Rene Saarsoo <nene@triin.net>
2016-08-11 11:19:03 +0300

    Drop extra sanity tests
    I think one simple sanity-test is enough.
    
    The other cases don't really add much value - they're essentially
    testing the same thing, and it's a thing we don't really expect to
    fail in the first place.

--- test/transform/multi-var.js
+++ test/transform/multi-var.js
@@ -3,39 +3,11 @@ const {expectTransform} = createTestHelpers(['multi-var']);
 
 describe('Multi-var', () => {
   describe('with only one variable per declaration', () => {
-    it('should not change anything(one declaration)', () => {
+    it('should not change anything', () => {
       expectTransform(
         'var x;'
       ).toReturn(
         'var x;'
-      );
-    });
-
-    it('should not change anything(multi declarations on one line)', () => {
-      expectTransform(
-        'var x;let y;'
-      ).toReturn(
-        'var x;let y;'
-      );
-
-      expectTransform(
-        'var x;let y;const a=100'
-      ).toReturn(
-        'var x;let y;const a=100'
-      );
-    });
-
-    it('should not change anything(multi declarations on multi lines)', () => {
-      expectTransform(
-        'var x;\nlet y;'
-      ).toReturn(
-        'var x;\nlet y;'
-      );
-
-      expectTransform(
-        'var x;\nlet y;\nconst a=100'
-      ).toReturn(
-        'var x;\nlet y;\nconst a=100'
       );
     });
   });
