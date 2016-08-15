9c912790dc07732193a06fad7871c38f0ceb1356
Rene Saarsoo <nene@triin.net>
2016-08-15 11:52:20 +0300

    Add watch task to run the transforms unit tests
    This is different from the old watch task that simply
    compiled Lebab files with Babel to lib/.  I find watching
    tests a more useful task to have.
    
    Not including tests for:
    
    - command line options
    - smoke test for the executable
    
    These are more of an integration/end2end tests.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,13 +2,4 @@ var grunt = require('grunt');
 require('load-grunt-tasks')(grunt);
 
 grunt.initConfig({
-  watch: {
-    options: {
-      spawn: false
-    },
-    scripts: {
-      files: ['src/**/*.js'],
-      tasks: ['default']
-    }
-  },
 });
