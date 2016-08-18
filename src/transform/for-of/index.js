168e5519ed790281a6efe7aa6b2e1f9051007c08
Rene Saarsoo <nene@triin.net>
2016-08-18 10:02:40 +0300

    Extract matchForLoop() to separate file
    Rename it to matchAliasedForLoop() as it only matches one very
    particular kind of the loop.

--- src/transform/for-of/index.js
+++ src/transform/for-of/index.js
@@ -1,19 +1,14 @@
 import _ from 'lodash';
 import traverser from '../../traverser';
 import isEqualAst from '../../utils/is-equal-ast';
-import {matchesAst, extract, matchesLength} from '../../utils/matches-ast';
+import matchAliasedForLoop from './match-aliased-for-loop';
 
 export default function(ast, logger) {
   traverser.replace(ast, {
     enter(node) {
-      const matches = matchForLoop(node);
+      const matches = matchAliasedForLoop(node);
 
-      if (
-        matches &&
-        consistentIndexVar(matches) &&
-        consistentArrayVar(matches) &&
-        !indexUsedInBody(matches)
-      ) {
+      if (matches && !indexUsedInBody(matches)) {
         return createForOf(matches);
       }
 
@@ -24,102 +19,9 @@ export default function(ast, logger) {
   });
 }
 
-function consistentIndexVar({index, indexComparison, indexIncrement, indexReference}) {
-  return isEqualAst(index, indexComparison) &&
-    isEqualAst(index, indexIncrement) &&
-    isEqualAst(index, indexReference);
-}
-
-function consistentArrayVar({array, arrayReference}) {
-  return isEqualAst(array, arrayReference);
-}
-
 function indexUsedInBody({body, index}) {
   return traverser.find(removeFirstBodyElement(body), (node) => isEqualAst(node, index));
 }
-
-var matchForLoop = matchesAst({
-  type: 'ForStatement',
-  init: {
-    type: 'VariableDeclaration',
-    declarations: matchesLength([
-      {
-        type: 'VariableDeclarator',
-        id: extract('index', {
-          type: 'Identifier',
-        }),
-        init: {
-          type: 'Literal',
-          value: 0,
-        }
-      }
-    ])
-  },
-  test: {
-    type: 'BinaryExpression',
-    operator: '<',
-    left: extract('indexComparison', {
-      type: 'Identifier',
-    }),
-    right: {
-      type: 'MemberExpression',
-      computed: false,
-      object: extract('array'),
-      property: {
-        type: 'Identifier',
-        name: 'length'
-      }
-    }
-  },
-  update: (node) => matchPlusPlus(node) || matchPlusOne(node),
-  body: extract('body', {
-    type: 'BlockStatement',
-    body: [
-      {
-        type: 'VariableDeclaration',
-        declarations: [
-          {
-            type: 'VariableDeclarator',
-            id: extract('item', {
-              type: 'Identifier',
-            }),
-            init: {
-              type: 'MemberExpression',
-              computed: true,
-              object: extract('arrayReference'),
-              property: extract('indexReference', {
-                type: 'Identifier',
-              })
-            }
-          }
-        ],
-        kind: extract('kind')
-      }
-    ]
-  })
-});
-
-// Matches <ident>++ or ++<ident>
-var matchPlusPlus = matchesAst({
-  type: 'UpdateExpression',
-  operator: '++',
-  argument: extract('indexIncrement', {
-    type: 'Identifier',
-  })
-});
-
-// Matches <ident>+=1
-var matchPlusOne = matchesAst({
-  type: 'AssignmentExpression',
-  operator: '+=',
-  left: extract('indexIncrement', {
-    type: 'Identifier',
-  }),
-  right: {
-    type: 'Literal',
-    value: 1
-  }
-});
 
 function createForOf({kind, item, array, body}) {
   return {
---
+++ src/transform/for-of/match-aliased-for-loop.js
@@ -0,0 +1,129 @@
+import isEqualAst from '../../utils/is-equal-ast';
+import {matchesAst, extract, matchesLength} from '../../utils/matches-ast';
+
+// Matches <ident>++ or ++<ident>
+const matchPlusPlus = matchesAst({
+  type: 'UpdateExpression',
+  operator: '++',
+  argument: extract('indexIncrement', {
+    type: 'Identifier',
+  })
+});
+
+// Matches <ident>+=1
+const matchPlusOne = matchesAst({
+  type: 'AssignmentExpression',
+  operator: '+=',
+  left: extract('indexIncrement', {
+    type: 'Identifier',
+  }),
+  right: {
+    type: 'Literal',
+    value: 1
+  }
+});
+
+// Matches for-loop
+// without checking the consistency of index and array variables:
+//
+// for (var index = 0; indexComparison < array.length; indexIncrement++) {
+//     var item = arrayReference[indexReference];
+//     ...
+// }
+const matchLooseForLoop = matchesAst({
+  type: 'ForStatement',
+  init: {
+    type: 'VariableDeclaration',
+    declarations: matchesLength([
+      {
+        type: 'VariableDeclarator',
+        id: extract('index', {
+          type: 'Identifier',
+        }),
+        init: {
+          type: 'Literal',
+          value: 0,
+        }
+      }
+    ])
+  },
+  test: {
+    type: 'BinaryExpression',
+    operator: '<',
+    left: extract('indexComparison', {
+      type: 'Identifier',
+    }),
+    right: {
+      type: 'MemberExpression',
+      computed: false,
+      object: extract('array'),
+      property: {
+        type: 'Identifier',
+        name: 'length'
+      }
+    }
+  },
+  update: (node) => matchPlusPlus(node) || matchPlusOne(node),
+  body: extract('body', {
+    type: 'BlockStatement',
+    body: [
+      {
+        type: 'VariableDeclaration',
+        declarations: [
+          {
+            type: 'VariableDeclarator',
+            id: extract('item', {
+              type: 'Identifier',
+            }),
+            init: {
+              type: 'MemberExpression',
+              computed: true,
+              object: extract('arrayReference'),
+              property: extract('indexReference', {
+                type: 'Identifier',
+              })
+            }
+          }
+        ],
+        kind: extract('kind')
+      }
+    ]
+  })
+});
+
+function isConsistentIndexVar({index, indexComparison, indexIncrement, indexReference}) {
+  return isEqualAst(index, indexComparison) &&
+    isEqualAst(index, indexIncrement) &&
+    isEqualAst(index, indexReference);
+}
+
+function isConsistentArrayVar({array, arrayReference}) {
+  return isEqualAst(array, arrayReference);
+}
+
+/**
+ * Matches for-loop that aliases current array element
+ * in the first line of the loop body:
+ *
+ *     for (var index = 0; index < array.length; index++) {
+ *         var item = array[index];
+ *         ...
+ *     }
+ *
+ * Extracts the following fields:
+ *
+ * - index - loop index identifier
+ * - array - array identifier or expression
+ * - item - identifier used to alias current array element
+ * - kind - the kind of <item>
+ * - body - the whole BlockStatement of for-loop body
+ *
+ * @param  {Object} node
+ * @return {Object}
+ */
+export default function(ast) {
+  const matches = matchLooseForLoop(ast);
+  if (matches && isConsistentIndexVar(matches) && isConsistentArrayVar(matches)) {
+    return matches;
+  }
+}
