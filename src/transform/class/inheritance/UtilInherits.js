fc514f65392bbec7d6d093a59e1785cf2f9a42c3
Christopher Jacobs <apexearth@gmail.com>
2016-09-10 22:01:09 +0000

    Change confusing if-statement in UtilInherits.js

--- src/transform/class/inheritance/UtilInherits.js
+++ src/transform/class/inheritance/UtilInherits.js
@@ -33,8 +33,8 @@ export default class UtilInherits {
    */
   process(node, parent) {
     var m;
-    if (this.discoverIdentifiers(node, parent)) {
-      return null;
+    if (isVarWithRequireCalls(node) && parent.type === 'Program') {
+      this.discoverIdentifiers(node, parent);
     }
     else if ((m = this.match(node))) {
       if (this.potentialClasses[m.className]) {
@@ -62,42 +62,34 @@ export default class UtilInherits {
    * @return {Boolean}
    */
   discoverIdentifiers(node, parent) {
-    if (isVarWithRequireCalls(node)) {
-      if (parent.type !== 'Program') {
-        return false;
-      }
-
-      var declaration = node.declarations.filter((dec) =>
-        matchesAst({
-          init: {
+    var declaration = node.declarations.filter((dec) =>
+      matchesAst({
+        init: {
+          callee: {
+            name: 'require'
+          },
+          arguments: (args) => args.length === 1 && args[0].value === 'util'
+        }
+      })(dec) ||
+      matchesAst({
+        init: {
+          object: {
             callee: {
               name: 'require'
             },
             arguments: (args) => args.length === 1 && args[0].value === 'util'
           }
-        })(dec) ||
-        matchesAst({
-          init: {
-            object: {
-              callee: {
-                name: 'require'
-              },
-              arguments: (args) => args.length === 1 && args[0].value === 'util'
-            }
-          }})(dec)
-      )[0];
+        }})(dec)
+    )[0];
 
-      if (declaration) {
-        if (matchesAst({init: {property: {name: 'inherits'}}})(declaration)) {
-          this.inheritsNode = declaration.id;
-        }
-        else {
-          this.utilNode = declaration.id;
-        }
-        return true;
+    if (declaration) {
+      if (matchesAst({init: {property: {name: 'inherits'}}})(declaration)) {
+        this.inheritsNode = declaration.id;
+      }
+      else {
+        this.utilNode = declaration.id;
       }
     }
-    return false;
   }
 
   /**
