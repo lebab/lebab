b29452cda52940ef037d571958bebaf112e72483
Joseph Frazier <1212jtraceur@gmail.com>
2017-03-28 16:50:49 -0400

    `no-strict` transform: preserve preceding comments
    Fixes https://github.com/lebab/lebab/issues/227

--- src/transform/noStrict.js
+++ src/transform/noStrict.js
@@ -1,10 +1,22 @@
 import traverser from '../traverser';
 import isString from '../utils/isString';
+import copyComments from '../utils/copyComments';
 
 export default function(ast) {
   traverser.replace(ast, {
-    enter(node) {
+    enter(node, parent) {
       if (node.type === 'ExpressionStatement' && isUseStrictString(node.expression)) {
+        const index = parent.body.indexOf(node);
+        const next = parent.body[index + 1];
+
+        if (next) {
+          copyComments({
+            from: node,
+            to: next,
+            prepend: true,
+          });
+        }
+
         this.remove();
       }
     }
