c0b7778c24eb13739f9991f63a7433e1ed727fdc
Rene Saarsoo <nene@triin.net>
2017-03-30 11:39:23 +0300

    Revert "Add `prepend` option to copyComments()"
    This reverts commit ad7d88ff4c0fab64ae46af0b9926193e093d1404.

--- src/utils/copyComments.js
+++ src/utils/copyComments.js
@@ -1,5 +1,5 @@
 /**
- * Appends (or prepends if specified) comments of one node to comments of another.
+ * Appends comments of one node to comments of another.
  *
  * - Modifies `to` node with added comments.
  * - Does nothing when there are no comments to copy
@@ -7,12 +7,9 @@
  *
  * @param  {Object} from Node to copy comments from
  * @param  {Object} to Node to copy comments to
- * @param  {Boolean} prepend prepend comments instead of appending
  */
-export default function copyComments({from, to, prepend}) {
+export default function copyComments({from, to}) {
   if (from.comments && from.comments.length > 0) {
-    const toComments = to.comments || [];
-    const bothComments = prepend ? [from.comments, toComments] : [toComments, from.comments];
-    to.comments = [].concat(...bothComments);
+    to.comments = (to.comments || []).concat(from.comments || []);
   }
 }
