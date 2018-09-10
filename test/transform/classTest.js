1b99ab1ddaaa1b9fd0ad13f072e385b15c8749ef
Rene Saarsoo <nene@triin.net>
2018-09-10 11:53:23 +0300

    Group class tests into three sections

--- test/transform/classTest.js
+++ test/transform/classTest.js
@@ -18,20 +18,6 @@ describe('Classes', () => {
     ]);
   });
 
-  it('should convert function declarations with prototype assignment to class', () => {
-    expectTransform(
-      'function MyClass() {\n' +
-      '}\n' +
-      'MyClass.prototype.method = function(a, b) {\n' +
-      '};'
-    ).toReturn(
-      'class MyClass {\n' +
-      '  method(a, b) {\n' +
-      '  }\n' +
-      '}'
-    );
-  });
-
   it('should convert static function declarations with assignment to static class methods', () => {
     expectTransform(
       'function MyClass() {\n' +
@@ -46,346 +32,366 @@ describe('Classes', () => {
     );
   });
 
-  it('should convert function variables with prototype assignment to class', () => {
-    expectTransform(
-      'var MyClass = function() {\n' +
-      '};\n' +
-      'MyClass.prototype.method = function() {\n' +
-      '};'
-    ).toReturn(
-      'class MyClass {\n' +
-      '  method() {\n' +
-      '  }\n' +
-      '}'
-    );
+  describe('assignment to prototype field', () => {
+    it('should convert to class (when function declaration used)', () => {
+      expectTransform(
+        'function MyClass() {\n' +
+        '}\n' +
+        'MyClass.prototype.method = function(a, b) {\n' +
+        '};'
+      ).toReturn(
+        'class MyClass {\n' +
+        '  method(a, b) {\n' +
+        '  }\n' +
+        '}'
+      );
+    });
+
+    it('should convert to class (when function variable used)', () => {
+      expectTransform(
+        'var MyClass = function() {\n' +
+        '};\n' +
+        'MyClass.prototype.method = function() {\n' +
+        '};'
+      ).toReturn(
+        'class MyClass {\n' +
+        '  method() {\n' +
+        '  }\n' +
+        '}'
+      );
+    });
+
+    it('should not convert arrow-function to class', () => {
+      expectNoChange(
+        'var MyClass = () => {\n' +
+        '  this.foo = 10;\n' +
+        '};\n' +
+        'MyClass.prototype.method = () => {\n' +
+        '  return this.foo;\n' +
+        '};'
+      );
+    });
+
+    it('should not convert arrow-function to method when it uses this', () => {
+      expectNoChange(
+        'function MyClass() {\n' +
+        '}\n' +
+        'MyClass.prototype.method = () => {\n' +
+        '  return this.foo;\n' +
+        '};'
+      );
+    });
+
+    it('should not convert arrow-function to method when it uses this inside nested arrow-function', () => {
+      expectNoChange(
+        'function MyClass() {\n' +
+        '}\n' +
+        'MyClass.prototype.method = () => {\n' +
+        '  return () => { this.foo(); };\n' +
+        '};'
+      );
+    });
+
+    it('should convert arrow-function to method when it does not use this', () => {
+      expectTransform(
+        'function MyClass() {\n' +
+        '}\n' +
+        'MyClass.prototype.method = () => {\n' +
+        '  return foo;\n' +
+        '};'
+      ).toReturn(
+        'class MyClass {\n' +
+        '  method() {\n' +
+        '    return foo;\n' +
+        '  }\n' +
+        '}'
+      );
+    });
+
+    it('should convert shorthand arrow-function to method when it does not use this', () => {
+      expectTransform(
+        'function MyClass() {\n' +
+        '}\n' +
+        'MyClass.prototype.method = () => foo;'
+      ).toReturn(
+        'class MyClass {\n' +
+        '  method() {\n' +
+        '    return foo;\n' +
+        '  }\n' +
+        '}'
+      );
+    });
+
+    it('should convert non-empty function to constructor method', () => {
+      expectTransform(
+        'function MyClass(a, b) {\n' +
+        '  this.params = [a, b];\n' +
+        '}\n' +
+        'MyClass.prototype.method = function(ma, mb) {\n' +
+        '};'
+      ).toReturn(
+        'class MyClass {\n' +
+        '  constructor(a, b) {\n' +
+        '    this.params = [a, b];\n' +
+        '  }\n' +
+        '\n' +
+        '  method(ma, mb) {\n' +
+        '  }\n' +
+        '}'
+      );
+    });
+
+    it('should preserve "async" keyword when converting methods', () => {
+      expectTransform(
+        'function MyClass(a, b) {\n' +
+        '  this.params = [a, b];\n' +
+        '}\n' +
+        'MyClass.prototype.method = async function(ma, mb) {\n' +
+        '};'
+      ).toReturn(
+        'class MyClass {\n' +
+        '  constructor(a, b) {\n' +
+        '    this.params = [a, b];\n' +
+        '  }\n' +
+        '\n' +
+        '  async method(ma, mb) {\n' +
+        '  }\n' +
+        '}'
+      );
+    });
+
+    it('should not convert non-anonymous functions to methods', () => {
+      expectNoChange(
+        'function MyClass() {\n' +
+        '}\n' +
+        'MyClass.prototype.method = method;\n' +
+        'function method(a, b) {\n' +
+        '}'
+      );
+    });
+
+    it('should ignore non-function assignments to prototype', () => {
+      expectTransform(
+        'function MyClass() {\n' +
+        '}\n' +
+        'MyClass.prototype.count = 10;\n' +
+        'MyClass.prototype.method = function() {\n' +
+        '};\n' +
+        'MyClass.prototype.hash = {foo: "bar"};'
+      ).toReturn(
+        'class MyClass {\n' +
+        '  method() {\n' +
+        '  }\n' +
+        '}\n' +
+        '\n' +
+        'MyClass.prototype.count = 10;\n' +
+        'MyClass.prototype.hash = {foo: "bar"};'
+      );
+    });
   });
 
-  it('should not convert arrow-function to class', () => {
-    expectNoChange(
-      'var MyClass = () => {\n' +
-      '  this.foo = 10;\n' +
-      '};\n' +
-      'MyClass.prototype.method = () => {\n' +
-      '  return this.foo;\n' +
-      '};'
-    );
+  describe('object assigned to prototype', () => {
+    it('should transform plain functions to methods', () => {
+      expectTransform(
+        'function MyClass() {\n' +
+        '}\n' +
+        'MyClass.prototype = {\n' +
+        '  methodA: function(a) {\n' +
+        '  },\n' +
+        '  methodB: function(b) {\n' +
+        '  }\n' +
+        '};'
+      ).toReturn(
+        'class MyClass {\n' +
+        '  methodA(a) {\n' +
+        '  }\n' +
+        '\n' +
+        '  methodB(b) {\n' +
+        '  }\n' +
+        '}'
+      );
+    });
+
+    it('should move over ES6 methods', () => {
+      expectTransform(
+        'function MyClass() {\n' +
+        '}\n' +
+        'MyClass.prototype = {\n' +
+        '  methodA(a) {\n' +
+        '  },\n' +
+        '  methodB(b) {\n' +
+        '  }\n' +
+        '};'
+      ).toReturn(
+        'class MyClass {\n' +
+        '  methodA(a) {\n' +
+        '  }\n' +
+        '\n' +
+        '  methodB(b) {\n' +
+        '  }\n' +
+        '}'
+      );
+    });
+
+    it('should move over getters/setters', () => {
+      expectTransform(
+        'function MyClass() {\n' +
+        '}\n' +
+        'MyClass.prototype = {\n' +
+        '  get methodA() {\n' +
+        '  },\n' +
+        '  set methodB(b) {\n' +
+        '  }\n' +
+        '};'
+      ).toReturn(
+        'class MyClass {\n' +
+        '  get methodA() {\n' +
+        '  }\n' +
+        '\n' +
+        '  set methodB(b) {\n' +
+        '  }\n' +
+        '}'
+      );
+    });
+
+    it('should ignore when it contains non-functions', () => {
+      expectNoChange(
+        'function MyClass() {\n' +
+        '}\n' +
+        'MyClass.prototype = {\n' +
+        '  method: function(a) {\n' +
+        '  },\n' +
+        '  foo: 10\n' +
+        '};'
+      );
+    });
+
+    it('should ignore when it contains arrow-functions that use this', () => {
+      expectNoChange(
+        'function MyClass() {\n' +
+        '}\n' +
+        'MyClass.prototype = {\n' +
+        '  method: () => this.foo,\n' +
+        '};'
+      );
+    });
+
+    it('should transform when it contains arrow-functions that do not use this', () => {
+      expectTransform(
+        'function MyClass() {\n' +
+        '}\n' +
+        'MyClass.prototype = {\n' +
+        '  method: () => foo,\n' +
+        '};'
+      ).toReturn(
+        'class MyClass {\n' +
+        '  method() {\n' +
+        '    return foo;\n' +
+        '  }\n' +
+        '}'
+      );
+    });
+
+    it('should preserve "async" when converting arrow-functions', () => {
+      expectTransform(
+        'function MyClass() {\n' +
+        '}\n' +
+        'MyClass.prototype = {\n' +
+        '  method: async () => foo,\n' +
+        '};'
+      ).toReturn(
+        'class MyClass {\n' +
+        '  async method() {\n' +
+        '    return foo;\n' +
+        '  }\n' +
+        '}'
+      );
+    });
   });
 
-  it('should not convert arrow-function to method when it uses this', () => {
-    expectNoChange(
-      'function MyClass() {\n' +
-      '}\n' +
-      'MyClass.prototype.method = () => {\n' +
-      '  return this.foo;\n' +
-      '};'
-    );
-  });
+  describe('Object.defineProperty', () => {
+    it('should convert setters and getters', () => {
+      expectTransform(
+        'function MyClass() {\n' +
+        '}\n' +
+        'Object.defineProperty(MyClass.prototype, "someAccessor", {\n' +
+        '  get: function () {\n' +
+        '    return this._some;\n' +
+        '  },\n' +
+        '  set: function (value) {\n' +
+        '    this._some = value;\n' +
+        '  }\n' +
+        '});'
+      ).toReturn(
+        'class MyClass {\n' +
+        '  get someAccessor() {\n' +
+        '    return this._some;\n' +
+        '  }\n' +
+        '\n' +
+        '  set someAccessor(value) {\n' +
+        '    this._some = value;\n' +
+        '  }\n' +
+        '}'
+      );
+    });
 
-  it('should not convert arrow-function to method when it uses this inside nested arrow-function', () => {
-    expectNoChange(
-      'function MyClass() {\n' +
-      '}\n' +
-      'MyClass.prototype.method = () => {\n' +
-      '  return () => { this.foo(); };\n' +
-      '};'
-    );
-  });
+    it('should ignore other options when converting get/set', () => {
+      expectTransform(
+        'function MyClass() {\n' +
+        '}\n' +
+        'Object.defineProperty(MyClass.prototype, "someAccessor", {\n' +
+        '  configurable: true,\n' +
+        '  enumerable: true,\n' +
+        '  set: function (value) {\n' +
+        '    this._some = value;\n' +
+        '  }\n' +
+        '});'
+      ).toReturn(
+        'class MyClass {\n' +
+        '  set someAccessor(value) {\n' +
+        '    this._some = value;\n' +
+        '  }\n' +
+        '}'
+      );
+    });
 
-  it('should convert arrow-function to method when it does not use this', () => {
-    expectTransform(
-      'function MyClass() {\n' +
-      '}\n' +
-      'MyClass.prototype.method = () => {\n' +
-      '  return foo;\n' +
-      '};'
-    ).toReturn(
-      'class MyClass {\n' +
-      '  method() {\n' +
-      '    return foo;\n' +
-      '  }\n' +
-      '}'
-    );
-  });
+    it('should ignore non-function property', () => {
+      expectNoChange(
+        'function MyClass() {\n' +
+        '}\n' +
+        'Object.defineProperty(MyClass.prototype, "propName", {\n' +
+        '  value: 10,\n' +
+        '  configurable: true,\n' +
+        '  writable: true\n' +
+        '});'
+      );
+    });
 
-  it('should convert shorthand arrow-function to method when it does not use this', () => {
-    expectTransform(
-      'function MyClass() {\n' +
-      '}\n' +
-      'MyClass.prototype.method = () => foo;'
-    ).toReturn(
-      'class MyClass {\n' +
-      '  method() {\n' +
-      '    return foo;\n' +
-      '  }\n' +
-      '}'
-    );
-  });
+    it('should transform arrow-function that does not use this', () => {
+      expectTransform(
+        'function MyClass() {\n' +
+        '}\n' +
+        'Object.defineProperty(MyClass.prototype, "getter", {\n' +
+        '  get: () => something\n' +
+        '});'
+      ).toReturn(
+        'class MyClass {\n' +
+        '  get getter() {\n' +
+        '    return something;\n' +
+        '  }\n' +
+        '}'
+      );
+    });
 
-  it('should convert non-empty function to constructor method', () => {
-    expectTransform(
-      'function MyClass(a, b) {\n' +
-      '  this.params = [a, b];\n' +
-      '}\n' +
-      'MyClass.prototype.method = function(ma, mb) {\n' +
-      '};'
-    ).toReturn(
-      'class MyClass {\n' +
-      '  constructor(a, b) {\n' +
-      '    this.params = [a, b];\n' +
-      '  }\n' +
-      '\n' +
-      '  method(ma, mb) {\n' +
-      '  }\n' +
-      '}'
-    );
-  });
-
-  it('should preserve "async" keyword when converting methods', () => {
-    expectTransform(
-      'function MyClass(a, b) {\n' +
-      '  this.params = [a, b];\n' +
-      '}\n' +
-      'MyClass.prototype.method = async function(ma, mb) {\n' +
-      '};'
-    ).toReturn(
-      'class MyClass {\n' +
-      '  constructor(a, b) {\n' +
-      '    this.params = [a, b];\n' +
-      '  }\n' +
-      '\n' +
-      '  async method(ma, mb) {\n' +
-      '  }\n' +
-      '}'
-    );
-  });
-
-  it('should not convert non-anonymous functions to methods', () => {
-    expectNoChange(
-      'function MyClass() {\n' +
-      '}\n' +
-      'MyClass.prototype.method = method;\n' +
-      'function method(a, b) {\n' +
-      '}'
-    );
-  });
-
-  it('should ignore non-function assignments to prototype', () => {
-    expectTransform(
-      'function MyClass() {\n' +
-      '}\n' +
-      'MyClass.prototype.count = 10;\n' +
-      'MyClass.prototype.method = function() {\n' +
-      '};\n' +
-      'MyClass.prototype.hash = {foo: "bar"};'
-    ).toReturn(
-      'class MyClass {\n' +
-      '  method() {\n' +
-      '  }\n' +
-      '}\n' +
-      '\n' +
-      'MyClass.prototype.count = 10;\n' +
-      'MyClass.prototype.hash = {foo: "bar"};'
-    );
-  });
-
-  it('should detect methods from object assigned directly to prototype', () => {
-    expectTransform(
-      'function MyClass() {\n' +
-      '}\n' +
-      'MyClass.prototype = {\n' +
-      '  methodA: function(a) {\n' +
-      '  },\n' +
-      '  methodB: function(b) {\n' +
-      '  }\n' +
-      '};'
-    ).toReturn(
-      'class MyClass {\n' +
-      '  methodA(a) {\n' +
-      '  }\n' +
-      '\n' +
-      '  methodB(b) {\n' +
-      '  }\n' +
-      '}'
-    );
-  });
-
-  it('should detect ES6 methods from object assigned directly to prototype', () => {
-    expectTransform(
-      'function MyClass() {\n' +
-      '}\n' +
-      'MyClass.prototype = {\n' +
-      '  methodA(a) {\n' +
-      '  },\n' +
-      '  methodB(b) {\n' +
-      '  }\n' +
-      '};'
-    ).toReturn(
-      'class MyClass {\n' +
-      '  methodA(a) {\n' +
-      '  }\n' +
-      '\n' +
-      '  methodB(b) {\n' +
-      '  }\n' +
-      '}'
-    );
-  });
-
-  it('should detect getters/setters from object assigned directly to prototype', () => {
-    expectTransform(
-      'function MyClass() {\n' +
-      '}\n' +
-      'MyClass.prototype = {\n' +
-      '  get methodA() {\n' +
-      '  },\n' +
-      '  set methodB(b) {\n' +
-      '  }\n' +
-      '};'
-    ).toReturn(
-      'class MyClass {\n' +
-      '  get methodA() {\n' +
-      '  }\n' +
-      '\n' +
-      '  set methodB(b) {\n' +
-      '  }\n' +
-      '}'
-    );
-  });
-
-  it('should ignore object assigned directly to prototype when it contains non-functions', () => {
-    expectNoChange(
-      'function MyClass() {\n' +
-      '}\n' +
-      'MyClass.prototype = {\n' +
-      '  method: function(a) {\n' +
-      '  },\n' +
-      '  foo: 10\n' +
-      '};'
-    );
-  });
-
-  it('should ignore object assigned directly to prototype when it contains arrow-functions that use this', () => {
-    expectNoChange(
-      'function MyClass() {\n' +
-      '}\n' +
-      'MyClass.prototype = {\n' +
-      '  method: () => this.foo,\n' +
-      '};'
-    );
-  });
-
-  it('should transform object assigned directly to prototype when it contains arrow-functions that do not use this', () => {
-    expectTransform(
-      'function MyClass() {\n' +
-      '}\n' +
-      'MyClass.prototype = {\n' +
-      '  method: () => foo,\n' +
-      '};'
-    ).toReturn(
-      'class MyClass {\n' +
-      '  method() {\n' +
-      '    return foo;\n' +
-      '  }\n' +
-      '}'
-    );
-  });
-
-  it('should preserve "async" when converting arrow-functions', () => {
-    expectTransform(
-      'function MyClass() {\n' +
-      '}\n' +
-      'MyClass.prototype = {\n' +
-      '  method: async () => foo,\n' +
-      '};'
-    ).toReturn(
-      'class MyClass {\n' +
-      '  async method() {\n' +
-      '    return foo;\n' +
-      '  }\n' +
-      '}'
-    );
-  });
-
-  it('should convert Object.defineProperty to setters and getters', () => {
-    expectTransform(
-      'function MyClass() {\n' +
-      '}\n' +
-      'Object.defineProperty(MyClass.prototype, "someAccessor", {\n' +
-      '  get: function () {\n' +
-      '    return this._some;\n' +
-      '  },\n' +
-      '  set: function (value) {\n' +
-      '    this._some = value;\n' +
-      '  }\n' +
-      '});'
-    ).toReturn(
-      'class MyClass {\n' +
-      '  get someAccessor() {\n' +
-      '    return this._some;\n' +
-      '  }\n' +
-      '\n' +
-      '  set someAccessor(value) {\n' +
-      '    this._some = value;\n' +
-      '  }\n' +
-      '}'
-    );
-  });
-
-  it('should ignore other options of Object.defineProperty when converting get/set', () => {
-    expectTransform(
-      'function MyClass() {\n' +
-      '}\n' +
-      'Object.defineProperty(MyClass.prototype, "someAccessor", {\n' +
-      '  configurable: true,\n' +
-      '  enumerable: true,\n' +
-      '  set: function (value) {\n' +
-      '    this._some = value;\n' +
-      '  }\n' +
-      '});'
-    ).toReturn(
-      'class MyClass {\n' +
-      '  set someAccessor(value) {\n' +
-      '    this._some = value;\n' +
-      '  }\n' +
-      '}'
-    );
-  });
-
-  it('should ignore Object.defineProperty of non-function property', () => {
-    expectNoChange(
-      'function MyClass() {\n' +
-      '}\n' +
-      'Object.defineProperty(MyClass.prototype, "propName", {\n' +
-      '  value: 10,\n' +
-      '  configurable: true,\n' +
-      '  writable: true\n' +
-      '});'
-    );
-  });
-
-  it('should transform Object.defineProperty with arrow-function that does not use this', () => {
-    expectTransform(
-      'function MyClass() {\n' +
-      '}\n' +
-      'Object.defineProperty(MyClass.prototype, "getter", {\n' +
-      '  get: () => something\n' +
-      '});'
-    ).toReturn(
-      'class MyClass {\n' +
-      '  get getter() {\n' +
-      '    return something;\n' +
-      '  }\n' +
-      '}'
-    );
-  });
-
-  it('should ignore Object.defineProperty with arrow-function that uses this', () => {
-    expectNoChange(
-      'function MyClass() {\n' +
-      '}\n' +
-      'Object.defineProperty(MyClass.prototype, "getter", {\n' +
-      '  get: () => this.something\n' +
-      '});'
-    );
+    it('should ignore arrow-function that uses this', () => {
+      expectNoChange(
+        'function MyClass() {\n' +
+        '}\n' +
+        'Object.defineProperty(MyClass.prototype, "getter", {\n' +
+        '  get: () => this.something\n' +
+        '});'
+      );
+    });
   });
 
   describe('comments', () => {
